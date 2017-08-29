cc775a99fb8a6eed3884c45e4f9b4c12727c4808
myousif1 <myousif1@gulls.salisbury.edu>
2017-08-29 14:09:33 -0400

    made some edits to delete some test code, add some test functions I can use later.

--- formTester.php
+++ formTester.php
@@ -16,41 +16,30 @@ $dbuser = "user"; //mysql username
 $dbpass = "password";//mysql pass
 $dbname = "database";//name of the database
 
-echo "<h2>we in here</h2>";
-
-
-$communicator = new DatabaseCommunicator($server,$dbuser,$dbpass,$dbname);
-
-if($communicator -> isConnected()){
-
-//	echo "We got connected <br>"; NOTE: WORKS
-
-	if($communicator -> constructNewQuery($communicator::INSERT, "Customer")){
-
-			//echo "we started building da new query <br>"; NOTE: WORKS
-
-
-		echo "This is the column array: <br>";
-
-		/*
-		foreach($communicator -> getCurrentTableFields() as $field)
-			echo $field . "<br>";
-		*/
-
-		if($communicator -> insertValue("firstname" , "Haytham") == false)
-			;
 
 
 
 
-	}else
-		echo "the new query we built didn't work \n" . $communicator -> getError();
 
 
+//debugging funcitons:
 
 
-}else
-	echo "there was some kind of error connecting \n" . $communicator -> getError();
+function printMessage($message)
+{
+	echo "<h2>" . $message . "</h2><br>";
+}
+function printArray($arr, $hasKeys = false){
+	if($hasKeys)
+		$keys = array_keys($arr);
+	for($i = 0; $i < sizeof($arr); $i++){
+		if($hasKeys)
+			printMessage($keys[$i] . " : " . $arr[$keys[$i]]);
+		else
+			printMessage($i . " : " . $arr[$i]);
+	}
+}
+
 
 
 ?>
